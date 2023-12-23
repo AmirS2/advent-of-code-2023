e8641dbb120ac0c442a7ce3ba7a3a03669bc0efb
Amir Shamsuddin <amir.shamsuddin@ly.st>
2023-12-24 02:38:24 +0800

    Day 1 part 1 solution

--- aoc2023/src/aoc2023/core.clj
+++ aoc2023/src/aoc2023/core.clj
@@ -7,4 +7,4 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (aoc2023.day-1.day-1/main 0))
+  (aoc2023.day-1.day-1/-main 0))
--- aoc2023/src/aoc2023/day_1/day_1.clj
+++ aoc2023/src/aoc2023/day_1/day_1.clj
@@ -1,9 +1,21 @@
-(ns aoc2023.day-1.day-1)
+(ns aoc2023.day-1.day-1
+  (:require [clojure.string :as str]))
 
-(defn main
+(defn first_digit [s] (re-find #"\d" s))
+
+(defn last_digit [s] (re-find #"\d" (str/reverse s)))
+
+(defn parse_line
+  [line]
+  (+ (* (Integer/parseInt (first_digit line)) 10)
+     (Integer/parseInt (last_digit line))))
+
+(defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (with-open [rdr (clojure.java.io/reader "src/aoc2023/day_1/input")]
-    (doseq [line (line-seq rdr)]
-      (println line)))
-  )
+    (doseq [line (line-seq rdr)] (println (parse_line line))))
+  (with-open [rdr (clojure.java.io/reader "src/aoc2023/day_1/input")]
+    (printf "Result is: %d\n"
+            (reduce #(+ %1 (parse_line %2)) 0 (line-seq rdr))))
+)
