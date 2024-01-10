53f232e7ea31fb055f4d162b762c6fd7709215f7
Amir Shamsuddin <AmirS2+git@gmail.com>
2024-01-10 11:26:08 +0000

    Day 19 part 1

--- aoc2023/src/aoc2023/day_19/part1.clj
+++ aoc2023/src/aoc2023/day_19/part1.clj
@@ -3,27 +3,46 @@
 
 (defn parse-rule [rule-str]
   (let [[_ xmas gtlt v target] (re-matches #"(?:([xmas])([<>])(\d+):)?(\w+)" rule-str)]
-    {:attr xmas :gtlt gtlt :val v :target target}))
+    {:attr xmas :gtlt gtlt :val (when v (Integer/parseInt v)) :target target}))
 
 (defn get-workflow [line]
-  (let [[_ wf-name & rules-strs] (re-matches #"(\w+)\{(?:([^,]*),)*(.*)\}" line)
-        rules (map parse-rule rules-strs)]
+  (let [[_ wf-name rules-strs] (re-matches #"(\w+)\{(.*)\}" line)
+        rules (map parse-rule (str/split rules-strs #","))]
     [wf-name rules]))
 
-(defn get-parts [lines]
-  lines)
+(defn get-part [line]
+  (let [xmas (map #(str/split % #"=") (str/split (re-find #"[^{}]+" line) #","))]
+    (into {} (map #(vector (first %) (Integer/parseInt (second %))) xmas))))
 
 (defn process [[line & remaining] workflows]
   (if (str/blank? line)
-    [workflows (get-parts remaining)]
+    [workflows (vec (map get-part remaining))]
     (recur remaining (conj workflows (get-workflow line)))))
+
+(defn get-wf-result [workflow part]
+  (loop [[{:keys [attr gtlt val target]} & remaining] workflow]
+    (if (or (not gtlt) ((case gtlt "<" < ">" >) (get part attr) val))
+      target
+      (recur remaining))))
+
+(defn is-accepted [workflows]
+  (fn [part]
+    (loop [wf "in"]
+      (let [next-wf (get-wf-result (get workflows wf) part)]
+        (cond
+          (= next-wf "A") true
+          (= next-wf "R") false
+          :else (recur next-wf))))))
 
 (defn -main
   "Read the input and solve it"
   [& args]
   (with-open [rdr (clojure.java.io/reader *in*)]
-    (let [result (process (line-seq rdr) {})]
-      (println result)
-      #_(printf "Sum is: %d\n" (apply + result))
+    (let [[workflows parts] (process (line-seq rdr) {})
+          accepted (filter (is-accepted workflows) parts)]
+      (dorun (map println workflows))
+      (dorun (map println parts))
+      (println "Accepted:" (str/join "\n" accepted))
+      (printf "Sum is: %d\n" (apply + (apply concat (map vals accepted))))
       ))
 )
